2e38e4668d1c870dfdb358006fdbc43051f89281
fsakurai <82249059+heyman1510185@users.noreply.github.com>
2025-06-05 21:41:49 +0900

    chore: update next and next-auth

--- apps/frontend/src/app/api/auth/[...nextauth]/route.ts
+++ apps/frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,1 @@
-import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import EmailProvider from "next-auth/providers/email";
-
-const prisma = new PrismaClient();
-
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
-});
-
-export { handler as GET, handler as POST };
+export { GET, POST } from "../../../../auth";
---
+++ apps/frontend/src/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import EmailProvider from "next-auth/providers/email";
+
+const prisma = new PrismaClient();
+
+export const { handlers: { GET, POST }, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
