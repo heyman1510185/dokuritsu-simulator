699465d8460f019c9c70630f185c7c7b591952f0
fsakurai <82249059+heyman1510185@users.noreply.github.com>
2025-06-05 21:39:19 +0900

    feat(frontend): add tailwind config

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', 'sans-serif'],
+        mono: ['var(--font-geist-mono)', 'monospace'],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
